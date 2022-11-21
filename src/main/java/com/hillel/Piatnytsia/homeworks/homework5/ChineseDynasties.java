01f841052902edc8653407481a8ceab39af0342d
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2022-11-22 00:13:14 +0200

    homework5

--- src/main/java/com/hillel/Piatnytsia/homeworks/homework5/ChineseDynasties.java
+++ src/main/java/com/hillel/Piatnytsia/homeworks/homework5/ChineseDynasties.java
@@ -1,5 +1,21 @@
 package com.hillel.Piatnytsia.homeworks.homework5;
 
 public class ChineseDynasties {
+    public static void main(String[] args){
+        int attackWarriorLi = 13;
+        int attackArcherLi = 24;
+        int attackRiderLi = 46;
+        int countSoldiersEachTypeLi = 860;
 
+        int attackWarriorMin = 9;
+        int attackArcherMin = 35;
+        int attackRiderMin = 12;
+        double countSoldiersEachTypeMin = countSoldiersEachTypeLi * 1.5 ;
+
+        int totalAttackArmyLi = countSoldiersEachTypeLi * (attackWarriorLi + attackArcherLi + attackRiderLi);
+        double totalAttackArmyMin = countSoldiersEachTypeMin * (attackWarriorMin + attackArcherMin + attackRiderMin);
+
+        System.out.println("Загальний показник атаки армії Лі = " + totalAttackArmyLi);
+        System.out.println("Загальний показник атаки армії Мінь = " + (int)totalAttackArmyMin);
+    }
 }
