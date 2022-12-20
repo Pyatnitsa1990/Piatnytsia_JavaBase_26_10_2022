49a83cd7a1777fd3d857dc3c415a345b2476b6fe
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2022-12-21 00:10:50 +0200

    homework11

--- src/main/java/com/hillel/Piatnytsia/homeworks/homework11/TransposeMatrix.java
+++ src/main/java/com/hillel/Piatnytsia/homeworks/homework11/TransposeMatrix.java
@@ -18,7 +18,9 @@ public class TransposeMatrix {
                     continue;
                 }
                 int[][] originalMatrix = new int[row][column];
-                int[][] transposeMatrixe = new int[column][row];
+                int[][] transposeMatrix = new int[column][row];
+                System.out.println();
+                System.out.println("Оригінальна матриця");
                 for (int i = 0; i < originalMatrix.length; i++) {
                     for (int j = 0; j < originalMatrix[i].length; j++) {
                         originalMatrix[i][j] = (int) (Math.random() * 11);
@@ -26,22 +28,18 @@ public class TransposeMatrix {
                     }
                     System.out.println();
                 }
-
-                for (int i = 0; i < transposeMatrixe.length; i++) {
-                    for (int j = 0; j < transposeMatrixe[i].length; j++) {
-                        int temp = transposeMatrixe[i][j];
-                        transposeMatrixe[i][j] = originalMatrix[j][i];
+                System.out.println();
+                System.out.println("Транспонована матриця");
+                for (int i = 0; i < transposeMatrix.length; i++) {
+                    for (int j = 0; j < transposeMatrix[i].length; j++) {
+                        int temp = transposeMatrix[i][j];
+                        transposeMatrix[i][j] = originalMatrix[j][i];
                         originalMatrix[j][i] = temp;
-                    }
-                }
-                System.out.println("after");
-
-                for (int i = 0; i < transposeMatrixe.length; i++) {
-                    for (int j = 0; j < transposeMatrixe[i].length; j++) {
-                        System.out.print(transposeMatrixe[i][j] + "\t");
+                        System.out.print(transposeMatrix[i][j] + "\t");
                     }
                     System.out.println();
                 }
+
                 counter++;
             } else {
                 System.out.println("Треба вказати числові значення");
