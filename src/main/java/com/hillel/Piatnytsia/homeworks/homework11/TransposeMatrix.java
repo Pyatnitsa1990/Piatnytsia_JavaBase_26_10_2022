6c5a24fac1e13cc2d1aa57e9829c54877042f60c
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2022-12-20 23:59:36 +0200

    homework10

--- src/main/java/com/hillel/Piatnytsia/homeworks/homework11/TransposeMatrix.java
+++ src/main/java/com/hillel/Piatnytsia/homeworks/homework11/TransposeMatrix.java
@@ -1,9 +1,9 @@
 package com.hillel.Piatnytsia.homeworks.homework11;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
-public class TransponeMatrix {
+public class TransposeMatrix {
     public static void main(String[] args) {
         System.out.println("Будь-ласка введіть розмір матриці");
         Scanner scanner = new Scanner(System.in);
@@ -18,7 +18,7 @@ public class TransponeMatrix {
                     continue;
                 }
                 int[][] originalMatrix = new int[row][column];
-                int[][] transponseMatrix = new int[column][row];
+                int[][] transposeMatrix = new int[column][row];
                 for (int i = 0; i < originalMatrix.length; i++) {
                     for (int j = 0; j < originalMatrix[i].length; j++) {
                         originalMatrix[i][j] = (int) (Math.random() * 11);
@@ -27,18 +27,18 @@ public class TransponeMatrix {
                     System.out.println();
                 }
 
-                for (int i = 0; i < transponseMatrix.length; i++) {
-                    for (int j = 0; j < transponseMatrix[i].length; j++) {
-                        int temp = transponseMatrix[i][j];
-                        transponseMatrix[i][j] = originalMatrix[j][i];
+                for (int i = 0; i < transposeMatrix.length; i++) {
+                    for (int j = 0; j < transposeMatrix[i].length; j++) {
+                        int temp = transposeMatrix[i][j];
+                        transposeMatrix[i][j] = originalMatrix[j][i];
                         originalMatrix[j][i] = temp;
                     }
                 }
                 System.out.println("after");
 
-                for (int i = 0; i < transponseMatrix.length; i++) {
-                    for (int j = 0; j < transponseMatrix[i].length; j++) {
-                        System.out.print(transponseMatrix[i][j] + "\t");
+                for (int i = 0; i < transposeMatrix.length; i++) {
+                    for (int j = 0; j < transposeMatrix[i].length; j++) {
+                        System.out.print(transposeMatrix[i][j] + "\t");
                     }
                     System.out.println();
                 }
